f2285d89b2c5e2d9e26f4de2fb694fc0592e915c
jugalnoob <sjugal126@gmnail.com>
2025-08-05 12:39:25 +0100

    'dsa'

---
+++ All_Array/Array_Algo/Array_Basic/Algo1/Insert.jsx
@@ -0,0 +1,41 @@
+
+
+1::Array Insert  ...................................
+:::::::::::::::::::::::
+et data=[ 9 , 8 , 7 , 6 , 5 , 4 , 3 , 2 , 1,0,100 ]
+console.log(data.length)
+let add=69;
+
+let post=0;
+
+for(let i=data.length-1; i>=0; i--){
+
+    if(i>=post){
+        
+        data[i+1]=data[i]
+    }if(i==post){
+
+        data[i]=add;
+    }
+
+}
+
+console.log(data)
+
+
+// ---> Third logic 
+
+var myArray = ["Element 1", "Element 2", "Element 3"];
+var elementsToInsert = ["New Element 1", "New Element 2"];
+var insertionIndexes = [1, 3];
+for (var i = 0; i < elementsToInsert.length; i++) {
+  var index = insertionIndexes[i];
+  myArray.splice(index, 0, elementsToInsert[i]);
+}
+console.log(myArray);
+
+
+// ---> Four Logic
+let data=[2 ,  3 , 4 , 5]
+data.splice(0 , 0 , 1)
+console.log(data)
